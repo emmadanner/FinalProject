81e71c4a5b5ff9a79b371b8855d7fcb746fcada9
channelblond <channelblond808@gmail.com>
2023-11-29 21:13:27 -0500

    amirs information

--- ProductWebMVC/ProductWebMVC/Data/ProductContext.cs
+++ ProductWebMVC/ProductWebMVC/Data/ProductContext.cs
@@ -16,8 +16,8 @@ namespace ProductWebMVC.Data
             new Product { Id = 1, year = "senior", name = "sam huseman", bd = "10/06/2000", program = "IT" },
                  new Product { Id = 2, year = "junior", name = "kyle doersam", bd = "02/08/2003", program = "IT" },
                  new Product { Id = 3, year = "senior", name = "sam huseman", bd = "10/06/2000", program = "IT" },
-                 new Product { Id = 4, year = "sophomore", name = "emma danner", bd = "04/09/2004", program = "IT" }
-                 new Product { Id = 4, year = "junior", name = "amir rai", bd = "07/19/2002", program = "IT" }
+                 new Product { Id = 4, year = "sophomore", name = "emma danner", bd = "04/09/2004", program = "IT" },
+                 new Product { Id = 5, year = "junior", name = "amir rai", bd = "07/19/2002", program = "IT" },
                   
                 );
         }
